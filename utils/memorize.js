581469af65596bdb4e6311e136e8da888c9c8371
Phong Lam <appstoregugle2310@gmail.com>
2019-04-07 16:02:22 +0700

    Practice BE Assignment

--- utils/memorize.js
+++ utils/memorize.js
@@ -3,11 +3,12 @@
  */
 module.exports = (fn) => {
     return function () {
-        const args = Array.prototype.slice.call(arguments);
+        const args = Array.prototype.slice.call(arguments),
+            key = JSON.stringify(args);
         fn.cache = fn.cache || {};
 
-        return fn.cache[args] ?
-            fn.cache[args] :
-            fn.cache[args] = fn.apply(this, args);
+        return fn.cache[key] ?
+            fn.cache[key] :
+            fn.cache[key] = fn.apply(this, args);
     }
 }
